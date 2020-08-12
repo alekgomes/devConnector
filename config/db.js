93ecc5c41a7c46c7f7eaedcc111b29d5f770119a
alekgomes <lucas.maistro@gmail.com>
2020-08-12 18:26:39 -0300

    criação de conexão com DB

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const mongoURI = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Banco de dados conectado...");
+  } catch (error) {
+    console.error(error);
+    // Exit process caso erro
+    process.exit(1)
+  }
+};
+
+module.exports = connectDB;

d84a2de687cfaac216d97efeaba50c73ecbcacc7
alekgomes <lucas.maistro@gmail.com>
2020-08-13 11:00:20 -0300

    criação de validdação para usuários

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-// @route   GET api/users
+// @route   POST api/users
 // @desc    Test route
 // @access  Public
-router.get("/", (req, res) => res.send("Users route!"));
+router.post(
+  "/",
+  [
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Enter a valid email"),
+    body("password")
+      .isLength({ min: 6 })
+      .withMessage("Password must have as least 6 chacaracters"),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.mapped()})
+    }
+    console.log(req.body);
+    res.send("Users route!");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ connectDB();
 
 app.get("/", (req, res) => res.send("API Running!"));
 
+// Init middleware
+app.use(express.json())
+
 // Definindo rotas
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
